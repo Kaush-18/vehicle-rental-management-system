67b2fc1abce1af0c13d4c3800d01ff847e17dfee
Kaushlendra Pratap Singh <kaush16122005@gmail.com>
2026-05-25 21:27:10 +0530

    route protected

--- frontend/src/components/ProtectedRoute.js
+++ frontend/src/components/ProtectedRoute.js
@@ -1,18 +1,27 @@
 import { Navigate } from "react-router-dom";
 
-function ProtectedRoute({ children }) {
+function ProtectedRoute({ children, allowedRoles }) {
 
-  const token =
-    localStorage.getItem("token");
+  const token = localStorage.getItem("token");
 
-  if (!token) {
+  const user = JSON.parse(
+    localStorage.getItem("user")
+  );
 
+  // Not logged in
+  if (!token || !user) {
     return <Navigate to="/login" />;
+  }
 
+  // Role check
+  if (
+    allowedRoles &&
+    !allowedRoles.includes(user.role)
+  ) {
+    return <Navigate to="/" />;
   }
 
   return children;
-
 }
 
 export default ProtectedRoute;
